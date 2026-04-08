97b0077fc38af8f7d9ed74cec3c8c2417f2cdb43
hds05 <duttsharmahimanshu96@gmail.com>
2026-04-08 17:28:11 +0530

    Add react-router-dom for routing

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Header(){
     return (
-        <header>
-            <div>
-                <h1>ChapterOne <sub>A ONLINE LIBRARY</sub></h1>
+        <header className="p-4 shadow-[0px_2px_5px_gray]">
+            <div className="m-2">
+                <h1 className="text-2xl">ChapterOne <sub className="text-sm">A online library</sub></h1>
             </div>
         </header>
     )
