cb078e696688d9ce200c498f41e9f18635ac2bf0
hds05 <duttsharmahimanshu96@gmail.com>
2026-04-08 17:13:57 +0530

    Installed TailwindCss in project. Removed all default css from App.css and index.css. Created a Header component in Components folder with basic structure and integrated in App.jsx

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(){
+    return (
+        <header>
+            <div>
+                <h1>ChapterOne <sub>A ONLINE LIBRARY</sub></h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
