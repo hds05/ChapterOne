c17e5e8b41bfcddf28499780dc5c6c1111e8b480
hds05 <duttsharmahimanshu96@gmail.com>
2026-04-09 23:27:40 +0530

    created custom hook books.js to fetch books data

---
+++ src/utils/books.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+
+export function fetchData(url){
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(()=>{
+        async function Data(){
+            try{
+                let response = await fetch(url)
+                let result = await response.json()
+                setData(result)
+                console.log(result);
+            }
+            catch(err){
+                setError(err)
+                console.error('there is an error of- ', err)
+            }            
+            finally{
+                setLoading(false)
+            }
+        }
+        Data()
+    }, [url])
+    return {data, loading, error}
+}
